9da163b5d01b19f4d307e87e7390f0f57de2ba28
Rio Rinaldi <riorinaldi@rios-macbook-pro.local>
2018-10-30 10:54:28 +0700

    Initial Commit

---
+++ todoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todoApp
+//
+//  Created by Rio Rinaldi on 30/10/18.
+//  Copyright © 2018 Rio Rinaldi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
